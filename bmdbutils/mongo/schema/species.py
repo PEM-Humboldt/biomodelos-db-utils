315f785a64ee013ad230ed708757daeabe6a4650
Marcelo <marcelo.villa.p@gmail.com>
2021-04-29 17:49:40 -0500

    Add species test schema.

---
+++ bmdbutils/mongo/schema/species.py
@@ -0,0 +1,12 @@
+"""
+MongoDB species collection schema.
+"""
+import pandas as pd
+
+fields = {
+    "acceptedNameUsage": {
+        "type": str,
+        "required": True,
+        "default": pd.NA
+    }
+}
