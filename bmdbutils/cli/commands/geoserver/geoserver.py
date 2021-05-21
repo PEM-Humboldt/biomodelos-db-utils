a3140db36bf9236210606881e021bf5049a6f83e
Erika Suárez Valencia <esuarez@humboldt.org.co>
2021-05-21 13:15:53 -0500

    Fix geoserver setup command

--- bmdbutils/cli/commands/geoserver/geoserver.py
+++ bmdbutils/cli/commands/geoserver/geoserver.py
@@ -7,10 +7,9 @@ import os
 import appdirs
 import click
 
+from bmdbutils.biomodelos.geoserver import Geoserver
 from .setup import setup
 from .upsert import upsert
-
-from bmdbutils.biomodelos.geoserver import Geoserver
 
 
 @click.group(
@@ -18,13 +17,26 @@ from bmdbutils.biomodelos.geoserver import Geoserver
 )
 @click.pass_context
 def geoserver(ctx):
-    config = configparser.ConfigParser()
-    config.read(os.path.join(appdirs.user_config_dir("bmdbutils"), "geoserver"))
-    ctx.obj = Geoserver(
-        config["LOCATION"]["url"],
-        config["CREDENTIALS"]["username"],
-        config["CREDENTIALS"]["password"],
-    )
+    if ctx.invoked_subcommand != "setup":
+        config = configparser.ConfigParser()
+        config.read(os.path.join(appdirs.user_config_dir("bmdbutils"), "geoserver"))
+        if (
+            len(config.sections()) <= 0
+            or not "LOCATION" in config.sections()
+            or not "CREDENTIALS" in config.sections()
+        ):
+            click.echo(
+                "Geoserver is misconfigured or hasn't been configured yet. "
+                "You must execute 'bmdbutils geoserver setup'"
+            )
+            return
+
+        ctx.obj = Geoserver(
+            config["LOCATION"]["url"],
+            config["CREDENTIALS"]["username"],
+            config["CREDENTIALS"]["password"],
+        )
 
 
 geoserver.add_command(setup)
+geoserver.add_command(upsert)
