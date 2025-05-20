788aa44b8dd939a28940b9c947966b90f787b3a3
jlhiguitac <jluis.higuita@gmail.com>
2025-05-20 16:27:18 -0500

    Added colors to the output in validation command

--- bmdbutils/cli/commands/mongo/validate.py
+++ bmdbutils/cli/commands/mongo/validate.py
@@ -15,8 +15,12 @@ from bmdbutils.biomodelos.mongo import Mongo
     help="Archivo CSV que contiene los registros de BioModelos",
 )
 def validate(csv_file):
-    result_validate_csv = Mongo.validate_csv_data_records(csv_file)
-    if result_validate_csv:
-        click.echo("✅ El archivo CSV posee el exquema necesario.")
+    validation, result_validate_csv = Mongo.validate_csv_data_records(csv_file)
+    if validation:
+        click.secho(result_validate_csv,
+                    fg="green", bold=True)
     else:
-        click.echo("⚠️ Lee atentamente y corrige el archivo CSV.")
+        click.secho("⚠️ Por favor leer atentamente y corregir el archivo CSV.",
+                    fg="yellow", bold=True)
+        click.secho(result_validate_csv,
+                    fg="red", bold=True)
