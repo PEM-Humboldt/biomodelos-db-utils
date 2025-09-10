628171698c9e5edcf4290a23173ac9b914db4c69
Jorge Higuita <jluis.higuita@gmail.com>
2025-09-10 09:44:24 -0500

    create sub-command models for stats: models, that is related to mongo class

---
+++ bmdbutils/cli/commands/stats/models.py
@@ -0,0 +1,27 @@
+"""
+$ bmdbutils stats models
+"""
+import click
+import configparser
+import os
+import appdirs
+
+from bmdbutils.biomodelos.mongo import Mongo
+
+pass_mongo = click.make_pass_decorator(Mongo)
+
+
+@click.command(
+    short_help="Obtener estadísticas de los modelos en la plataforma."
+)
+
+@pass_mongo
+def models(mongo):
+    """Obtener las estadisticas de modelos en la plataforma.
+    """
+    cnx = mongo.mongo_connection()
+    data = mongo.models_stats(cnx)
+    for doc in data:
+        for key, value in doc.items():
+           click.secho(f"Se encontraron {value} modelos en {key}.", fg="white")
+    cnx.close()  
